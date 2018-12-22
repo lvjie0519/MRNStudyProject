f4310fb92cf9f955da4c364bdb1842bca3e15c4c
lvjie1 <lvjie1@xiaomi.com>
2018-12-22 15:28:44 +0800

    初始化RN环境

--- app/src/main/java/com/cn/example/project/MainActivity.java
+++ app/src/main/java/com/cn/example/project/MainActivity.java
@@ -1,7 +1,11 @@
 package com.cn.example.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.cn.example.project.rn.MyFirstRnActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goRnPage(View view){
+        Intent intent = new Intent(this, MyFirstRnActivity.class);
+        startActivity(intent);
+    }
+
 }
